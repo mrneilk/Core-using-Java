26683ddd9540d116f73faffc780751b5005e90b9
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2026-09-03 21:11:29 +0530

    Add comment to main method in HelloWorld.java

--- Legacy/DAY1/HelloWorld.java
+++ Legacy/DAY1/HelloWorld.java
@@ -1,7 +1,7 @@
 package DAY1;
 
 class HelloWorld{
-    public static void main(String[] args) {
+    public static void main(String[] args) { // main
         System.out.print("Hello World");
     }
 }
